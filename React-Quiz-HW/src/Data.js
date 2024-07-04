e7daa037c2c6f64c04ffa07b07a04227f3cc79e1
Eli <144692245+Gamzayeff@users.noreply.github.com>
2024-07-04 08:59:17 +0400

    Add files via upload

---
+++ React-Quiz-HW/src/Data.js
@@ -0,0 +1,128 @@
+export const questions = 
+[
+    {
+        id:1,
+        question:"Birinci toyuq yoxsa yumurta yaranib?",
+        answer1:"Toyuq",
+        answer2:"Yumurta",
+        answer3:"Xoruz",
+        trueAnswer:"Toyuq",
+        bgColor:"grey",
+        color:"black",
+        e:"",
+        isCheck:false
+    },
+    {
+        id:2,
+        question:"JavaScript-i kim yaratib?",
+        answer1:"Elo Musk",
+        answer2:"Brendan Eyx",
+        answer3:"Lary Page",
+        trueAnswer:"Al Nassr FC",
+        bgColor:"grey",
+        color:"black",
+        e:"",
+        isCheck:false
+
+    },
+    {
+        id:3,
+        question:"Dunyanin en boyuk denizi hansidir?",
+        answer1:"Flippin",
+        answer2:"Sakit",
+        answer3:"Hind",
+        trueAnswer:"Flippin",
+        bgColor:"grey",
+        color:"black",
+        e:"",
+        isCheck:false
+
+    },
+    {
+        id:4,
+        question:"Xezer Denizdir yoxsa Gol",
+        answer1:"Deniz",
+        answer2:"Okean",
+        answer3:"Gol",
+        trueAnswer:"1999",
+        bgColor:"grey",
+        color:"black",
+        e:"",
+        isCheck:false
+
+    },
+    {
+        id:5,
+        question:"BMW necenci ilde yaranib?",
+        answer1:"7 mart 1916",
+        answer2:"11 mart 1916",
+        answer3:"7 mart 1917",
+        trueAnswer:"7 mart 1916",
+        bgColor:"grey",
+        color:"black",
+        e:"",
+        isCheck:false
+
+    },
+    {
+        id:6,
+        question:"Google kim yaratdi",
+        answer1:"Larry Page",
+        answer2:"Elon Musk",
+        answer3:"Jeef Bezos",
+        trueAnswer:"Larry Page",
+        bgColor:"grey",
+        color:"black",
+        e:"",
+        isCheck:false
+    },
+    {
+        id:7,
+        question:"Dunyanin en varli insani kimdir?",
+        answer1:"Jeef Bezos",
+        answer2:"Bill Gates",
+        answer3:"Elon Musk",
+        trueAnswer:"Elon Musk",
+        bgColor:"grey",
+        color:"black",
+        e:"",
+        isCheck:false
+    },
+    {
+        id:8,
+        question:"En Suretli sedan hansidir?",
+        answer1:"CLS 63S AMG",
+        answer2:"M5 F90 G-POWER",
+        answer3:"AUDI RS7 MTM",
+        trueAnswer:"M5 F90 G-POWER",
+        bgColor:"grey",
+        color:"black",
+        e:"",
+        isCheck:false
+    },
+    {
+        id:9,
+        question:"Azerbaycanin paytaxti hansidir?",
+        answer1:"Gence",
+        answer2:"Baki",
+        answer3:"Qebele",
+        trueAnswer:"Baki",
+        bgColor:"grey",
+        color:"black",
+        e:"",
+        isCheck:false
+    },
+    {
+        id:10,
+        question:"Ilk defe PC ne zaman yaranmiwdir",
+        answer1:"1970",
+        answer2:"1971",
+        answer3:"1978",
+        trueAnswer:"1970",
+        bgColor:"grey",
+        color:"black",
+        e:"",
+        isCheck:false
+    }
+
+]
